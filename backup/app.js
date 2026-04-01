e96949b235fe678e93b4556793037f5959337394
Tennol_Miseru <tiannuopm@gmail.com>
2026-04-01 17:38:51 +0800

    ignore backup folder

--- backup/app.js
+++
@@ -1,7 +0,0 @@
-﻿// Engineering calendar entrypoint (split from the previous monolithic app.js)
-
-document.addEventListener("DOMContentLoaded", () => {
-  const ns = window.CamenCalendar;
-  const ctx = ns.core.createContext();
-  ns.bindings.init(ctx);
-});
